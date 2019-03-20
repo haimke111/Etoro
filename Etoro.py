224c3b593dc67620fc33894725f76a99f4720d22
haimke111 <haimke8200@gmail.com>
2019-03-21 00:04:15 +0200

    fixing script

--- Etoro.py
+++ Etoro.py
@@ -5,19 +5,9 @@ import sys
 #18.3.2019
 
 server = jenkins.Jenkins('http://localhost:8080/', username='haim', password='Tempo20192019!')
-user = server.get_whoami()
+job_name = server.get_job_name(sys.argv[1])
+print ("Job name is: ", job_name)
 
-version = server.get_version()
-print('Hello %s from Jenkins %s' % (user['fullName'], version))
-jobs = server.get_job_info(sys.argv[1],0,True)
-
-
-build_info = server.get_build_info(sys.argv[1],int(sys.argv[2]))
-
-duration = build_info["duration"]
-status = build_info["result"]
-print("Status: ",status)
-print("Duration(ms): ",duration)
 
 
 
