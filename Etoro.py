20e1354adf1d742740f8a0a5a0ed034fa847dc83
haimke111 <haimke8200@gmail.com>
2019-03-24 23:06:28 +0200

    fixing script

--- Etoro.py
+++ Etoro.py
@@ -4,10 +4,15 @@ import sys
 #Created by Haimke
 #18.3.2019
 
-server = jenkins.Jenkins('http://localhost:8080/', username='haim', password='Tempo20192019!')
-job_name = server.get_job_name(sys.argv[1])
-print ("Job name is: ", job_name)
-
+Script_Success_Index = sys.argv[3]
+counter = sys.argv[2]
+if counter % Script_Success_Index == 0:
+    server = jenkins.Jenkins('http://localhost:8080/', username='haim', password='Tempo20192019!')
+    job_name = server.get_job_name(sys.argv[1])
+    print ("Job name is: ", job_name)
+    sys.exit(0)
+else:
+    sys.exit(1)
 
 
 
